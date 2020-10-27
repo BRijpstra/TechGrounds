51d5c59a8c631dfdd2b95d335144f72e9bf76550
Berend Rijpstra <BRijpstra@gmail.com>
2020-10-27 01:01:05 +0100

    + Guess the word
    + added visible code snippets for some of the condition assignments
    + retrospective week 8

--- JavaScript/RPS/scripts.js
+++ JavaScript/RPS/scripts.js
@@ -1,2 +1,36 @@
 document.getElementById("readybanner").onclick = function(e) {    e.target.style.display = 'none';
 }
+
+let playerScore = 0;
+let CPUScore = 0;
+
+let weapons = [{ name:'rock', icon: '<i class="fas fa-hand-rock"></i>'},{ name:'paper', icon: '<i class="fas fa-hand-paper"></i>'}, {name:'scissors', icon:'<i class="fas fa-hand-scissors"></i>'}];
+    let randomItem = items[Math.floor(Math.random()*3)].name;
+
+function choose(obj) {
+ 
+
+
+
+
+
+
+
+
+
+
+    let choice = obj.children[1].innerHTML;
+
+            if (randomWord == choice) {
+                document.getElementById('label').textContent = 'It was [' + randomWord + '] indeed! You might be clearvoyant!';     
+            }
+
+            else if (randomWord !== choice && count < 3) {
+                document.getElementById('label').textContent = 'Not really, guess again'; 
+            }
+            else {
+                document.getElementById('label').textContent = "Strrrrrike three: you're out! The word was [" + randomWord + ']. GAME OVER.';
+            
+            }
+            }
+    
