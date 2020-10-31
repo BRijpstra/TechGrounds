4a3661ed15aefa94e33ee8dabd99d768a820171c
Berend Rijpstra <BRijpstra@gmail.com>
2020-10-31 17:51:55 +0100

    AnimatePackage test

--- JavaScript/RPS/scripts.js
+++ JavaScript/RPS/scripts.js
@@ -1,10 +1,82 @@
 document.getElementById("readybanner").onclick = function(e) {    e.target.style.display = 'none';
 }
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+
+class Game {
+    constructor(container, icons) {
+        this icons = icons;
+
+        this.title = document.createElement('h3');
+        this.title.innertext = 'Make choice';
+        container.appendChild(this.title);
+
+
+
+
+
+
+      const container = document.querySelector('#container');
+       
+      icons.forEach((icon, index) => { 
+        const button = document.createElement("button");
+        button.innertext = icon;
+        container.appendChild(button);
+
+        button.onclick = () => this.play(index);
+        
+        ))
+        }
+
+        play(hand) {
+            const computer = getRandomHand();
+            const winner = getWinner(hand, computer);
+
+
+            if (winner === hand) {
+                console.log("gewonnen!")}
+                else if (winner === computer) {
+                                    }
+                else {
+                    geenwinnnaar
+                }
+            }
+            console.log(hand, computer, getWinner(hand, computer));
+    }
+
+    function getRandomHand() {
+        return Math.floor(Math.random()*3);
+    }
+    function getWinner(hand1, hand2) {
+        if (hand1 === hand1) return undefined;
+
+        switch(hand1) {
+            case rock:
+                return hand2 === scissors ? hand1 : hand2;
+            case paper:
+                return hand2 === rock? hand1 : hand2;
+            case scissors:
+                return hand2 === paper? hand1 : hand2;
+        }
+    }
+}
+
+new Game(document.querySelector('#container'), [👊, ✋, 🖖]);
+
+
+
+
+
+
+
+
 
 let scorePlayer = 0;
 let scoreCPU = 0;
 
-let weapons = [rock, paper, scissors];
+let weapons = [👊, ✋, 🖖];
 let randomPRS = weapons[Math.floor(Math.random()*3)];
 
 function choose(obj) {
